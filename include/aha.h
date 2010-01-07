a9d688258870b42e93d7d239c27721330dfea941
Gerard Wagener <gerard@ncr0.(none)>
2010-01-07 20:45:51 +0100

    Forgot to include additional header file

---
+++ include/aha.h
@@ -0,0 +1,12 @@
+#ifndef AHA
+#define AHA
+/*FIXME use AHA name space */
+#define MAX_DUMP_BUF 512
+struct ReplyMessage{
+    int block;
+    int exitcode;
+    int substitue;
+    int insult;
+};
+
+#endif
