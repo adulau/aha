2f50e920ff326c426413b47af8fa2b82276dbba5
Gerard Wagener <gerard@ncr0.(none)>
2010-01-12 14:17:27 +0100

    Add new shared header file

---
+++ arch/um/include/shared/aha.h
@@ -0,0 +1,36 @@
+#ifndef AHA
+#define AHA
+#include "linux/kernel.h" /* printk is declared there */
+//#include "linux/gfp.h" /* GFP_KERNEL */
+
+#include "linux/stddef.h"
+//#include "linux/fs.h"
+//#include "linux/smp_lock.h"
+#include "linux/ptrace.h" /* access to kmalloc */
+//#include "linux/sched.h"
+//#include "asm/current.h"
+//#include "asm/processor.h"
+//#include "asm/uaccess.h"
+//#include "as-layout.h"
+//#include "mem_user.h"
+//#include "skas.h"
+#include "os.h"
+#include "linux/delay.h"
+
+/*FIXME use AHA name space */
+#define MAX_DUMP_BUF 512
+struct ReplyMessage{
+    int block;
+    int exitcode;
+    int substitue;
+    int insult;
+};
+
+int aha_create_filename(char *fn, int size);
+char* aha_dump_execve(char __user *file, char __user *__user *argv,\
+        char __user *__user *env);
+
+void aha_handle_insult_messages(struct ReplyMessage *msg, char __user* file,\
+                            char __user* __user* argv);
+void aha_get_reply_message(char* key, struct ReplyMessage *msg);
+#endif
